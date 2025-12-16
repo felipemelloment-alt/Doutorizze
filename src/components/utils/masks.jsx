f8b050af190f99d7777b152c3e1a0537a41a1fc9
Felipe MELLO <mellofelipe17@gmail.com>
2025-12-16 05:23:07 +0000

    File changes

---
+++ src/components/utils/masks.jsx
@@ -0,0 +1,37 @@
+// CPF Mask: XXX.XXX.XXX-XX
+export function maskCPF(value) {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d{1,2})/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+}
+
+// CNPJ Mask: XX.XXX.XXX/XXXX-XX
+export function maskCNPJ(value) {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1.$2")
+    .replace(/(\d{3})(\d)/, "$1/$2")
+    .replace(/(\d{4})(\d)/, "$1-$2")
+    .replace(/(-\d{2})\d+?$/, "$1");
+}
+
+// Phone Mask: (XX) XXXXX-XXXX
+export function maskPhone(value) {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{2})(\d)/, "($1) $2")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/(-\d{4})\d+?$/, "$1");
+}
+
+// CEP Mask: XXXXX-XXX
+export function maskCEP(value) {
+  return value
+    .replace(/\D/g, "")
+    .replace(/(\d{5})(\d)/, "$1-$2")
+    .replace(/(-\d{3})\d+?$/, "$1");
+}
