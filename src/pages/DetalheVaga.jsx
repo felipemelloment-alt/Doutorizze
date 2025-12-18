401d9799abed4898ceef536c7db2fbb947e456cd
Felipe MELLO <mellofelipe17@gmail.com>
2025-12-18 03:44:20 +0000

    File changes

--- src/pages/DetalheVaga.jsx
+++ src/pages/DetalheVaga.jsx
@@ -223,13 +223,14 @@ export default function DetalheVaga() {
                     {unit.media_avaliacoes?.toFixed(1) || "0.0"} ({unit.total_avaliacoes || 0})
                   </span>
                 </div>
-              </div>
-              <button
-                onClick={() => navigate(createPageUrl("PerfilClinica"))}
+                </div>
+                {/* TODO: Adicionar botão "Ver Clínica" quando página PerfilClinicaPublico estiver pronta */}
+                {/* <button
+                onClick={() => navigate(createPageUrl("PerfilClinicaPublico") + "/" + unit.id)}
                 className="px-4 py-2 border-2 border-gray-300 text-gray-700 font-semibold rounded-2xl hover:border-yellow-400 hover:text-yellow-600 transition-all whitespace-nowrap"
-              >
+                >
                 Ver Clínica
-              </button>
+                </button> */}
             </div>
           </motion.div>
         )}
