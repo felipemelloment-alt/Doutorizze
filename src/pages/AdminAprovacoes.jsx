678561e66bf4b243aa6de2ddd357683fde539a8c
Felipe MELLO <mellofelipe17@gmail.com>
2025-12-19 03:58:15 +0000

    File changes

--- src/pages/AdminAprovacoes.jsx
+++ src/pages/AdminAprovacoes.jsx
@@ -246,7 +246,9 @@ export default function AdminAprovacoes() {
         ? "CLINICA"
         : cadastro.tipo === "FORNECEDOR"
         ? "FORNECEDOR"
-        : "HOSPITAL";
+        : cadastro.tipo === "HOSPITAL"
+        ? "HOSPITAL"
+        : "INSTITUICAO";
 
       await base44.entities.Notification.create({
         destinatario_id: cadastro.user_id,
